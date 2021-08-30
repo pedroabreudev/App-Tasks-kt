25a73dd1bcd0f593cfbe57214d452fe05469536e
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-08-30 17:04:32 -0400

    Commit antes de atualizar as libs do projeto.

--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,13 +2,18 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mPersonRepository = PersonRepository()
+    //private lateinit var mPersonRepository: PersonRepository
 
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
+        mPersonRepository.login(email, password)
     }
 
     /**
