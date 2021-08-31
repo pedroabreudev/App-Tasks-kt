ae1e26891832bf456a52f4d9ad87dbfe05fedc0c
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-08-31 14:31:06 -0400

    Salvando o usuário já logado

--- app/src/main/java/com/example/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/example/tasks/view/LoginActivity.kt
@@ -65,6 +65,11 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
             }
         })
+        mViewModel.loggedUser.observe(this, Observer {
+            if (it){
+                startActivity(Intent(this,MainActivity::class.java))
+            }
+        })
     }
 
     /**
